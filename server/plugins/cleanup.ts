f4fbdafa0f1b86fd7fed22d7b725f1c6e3163ef5
Rottie#7658 <89132@glr.nl>
2023-11-15 22:00:33 +0100

    Removed modules that weren't used

--- server/plugins/cleanup.ts
+++ server/plugins/cleanup.ts
@@ -1,16 +1,12 @@
 import { useScheduler } from "#scheduler"
 const config = useRuntimeConfig()
-const SocketUrl: any = config.public.Environment == "Production" ? config.public.SocketUrl : `http://localhost:${config.public.SocketPORT}`
-
-import io from 'socket.io-client';
-const socket = io(SocketUrl);
 
 export default defineNitroPlugin(() => {
     const scheduler = useScheduler();
 
     scheduler.run(async () => {
         //await Audit.deleteMany({})
-        socket.emit('AsyncRefresh', [{}])
+        
     }).cron(`0 0 * * 0,2`)
 
 })
