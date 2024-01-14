82daf7991e25d64c0c34d507a51eb76916707499
Rottie#7658 <89132@glr.nl>
2024-01-14 13:48:51 +0100

    Refactor KeepAlive plugin to use setTimeout for reconnection + changed the cron to 2 minutes

--- server/plugins/KeepAlive.ts
+++ server/plugins/KeepAlive.ts
@@ -1,17 +1,16 @@
 import { useScheduler } from "#scheduler"
-import http from 'http';
 
 const config = useRuntimeConfig()
 export default defineNitroPlugin(() => {
     const scheduler = useScheduler();
-    scheduler.run(async () => {
+    scheduler.run(() => {
         console.log(`\n\x1b[34mℹ\x1b[0m Attempting to reconnect to server!`)
-
-        await fetch(`${config.ServerUrl}`).then(res => {
-            console.log(`\x1b[32m✔\x1b[0m Reconnected successfully!`)
-        }).catch(err => console.error(err))
-        
-    }).everySeconds(10);
+        setTimeout(async () => {
+            await fetch(`${config.ServerUrl}`).then(res => {
+                console.log(`\x1b[32m✔\x1b[0m Reconnected successfully!`)
+            }).catch(err => console.error(err))
+        }, 1000);
+    }).everyMinutes(2)
 })
 
 
