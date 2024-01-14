06a30f11f2c2ce273c163883defbe7bfa9f217b5
Rottie#7658 <89132@glr.nl>
2024-01-14 13:32:52 +0100

    Refactor KeepAlive plugin to use fetch API

--- server/plugins/KeepAlive.ts
+++ server/plugins/KeepAlive.ts
@@ -7,13 +7,10 @@ export default defineNitroPlugin(() => {
     scheduler.run(async () => {
         console.log(`\n\x1b[34mℹ\x1b[0m Attempting to reconnect to server!`)
 
-        const req = http.request(`${config.ServerUrl}`, (res) => {
-            res.on('data', () => { console.log(`\x1b[32m✔\x1b[0m Reconnected successfully!`);})
-        });
-
-        req.on('error', (error) => { console.error(error); }); 
-        req.end();
-
+        await fetch(`${config.ServerUrl}`).then(res => {
+            console.log(`\x1b[32m✔\x1b[0m Reconnected successfully!`)
+        }).catch(err => console.error(err))
+        
     }).everySeconds(10);
 })
 
